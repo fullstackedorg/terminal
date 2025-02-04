ddce354b3e32deb3151db3dd39043137d8fcab70
cplepage <lepage.charlesphilippe@gmail.com>
2025-02-04 10:01:10 -0500

    better approach for intergration

--- index.ts
+++ index.ts
@@ -1,107 +1,16 @@
-import "@xterm/xterm/css/xterm.css";
-import { Terminal } from "@xterm/xterm";
-import { FitAddon } from "@xterm/addon-fit";
-import LocalEchoController from "./local-echo";
+import StackNav from "@fullstacked/stack-navigation";
+import { createTerminal } from "./terminal";
 import eruda from "eruda";
 eruda.init();
 
-const terminal = new Terminal();
-const fitAddon = new FitAddon();
-terminal.loadAddon(fitAddon);
+const sn = new StackNav();
 
-const fit = () => {
-    document.documentElement.style.height = window.visualViewport.height + "px";
-    document.body.style.height = window.visualViewport.height + "px";
-    const keyboardExtHeight = keyboardExtension.getBoundingClientRect().height;
-    terminalContainer.style.height =
-        window.visualViewport.height - keyboardExtHeight + "px";
-    fitAddon.fit();
-}
-window.addEventListener("resize", fit);
+const view = document.createElement("main");
+sn.navigate(view, {
+    bgColor: "coral"
+});
 
 const terminalContainer = document.createElement("div");
-terminalContainer.id = "terminal-container";
-document.body.append(terminalContainer);
-terminal.open(terminalContainer);
-
-terminal.focus();
-
-const keyboardExtension = document.createElement("div");
-keyboardExtension.id = "keyboard-extension";
-
-const tabButton = document.createElement("button");
-tabButton.innerText = "tab";
-tabButton.onclick = () => {
-    terminal.focus();
-    terminal.textarea.dispatchEvent(
-        new KeyboardEvent("keydown", {
-            key: "Tab",
-            keyCode: 9,
-            code: "Tab",
-            which: 9,
-        }),
-    );
-};
-const leftButton = document.createElement("button");
-leftButton.innerText = "<";
-leftButton.onclick = () => {
-    terminal.focus();
-    terminal.textarea.dispatchEvent(
-        new KeyboardEvent("keydown", {
-            key: "ArrowLeft",
-            keyCode: 37,
-            code: "ArrowLeft",
-            which: 37,
-        }),
-    );
-};
-const rightButton = document.createElement("button");
-rightButton.innerText = ">";
-rightButton.onclick = () => {
-    terminal.focus();
-    terminal.textarea.dispatchEvent(
-        new KeyboardEvent("keydown", {
-            key: "ArrowRight",
-            keyCode: 39,
-            code: "ArrowRight",
-            which: 39,
-        }),
-    );
-};
-keyboardExtension.append(tabButton, leftButton, rightButton);
-
-document.body.append(keyboardExtension);
-
-let lastHeight = window.visualViewport.height;
-const checkHeight = () => {
-    const currentHeight = window.visualViewport.height;
-
-    if (currentHeight !== lastHeight) {
-        window.dispatchEvent(new Event("resize"));
-    }
-
-    lastHeight = currentHeight;
-
-    window.requestAnimationFrame(checkHeight);
-};
-checkHeight();
-
-const localEcho = new LocalEchoController(terminal);
-
-localEcho.addAutocompleteHandler(() => ["autocomplete", "all"]);
-
-function loop() {
-    localEcho
-        .read("~$ ")
-        .then(async (cmd) => {
-            localEcho.print(`Hello`);
-            await new Promise(res => setTimeout(res, 2000));
-            localEcho.clearInput()
-            localEcho.println(`Unknown command [${cmd.trim()}]`)
-            loop();
-        })
-        .catch(console.log);
-}
-
-loop();
-fit();
+terminalContainer.classList.add("terminal-container");
+view.append(terminalContainer);
+createTerminal(terminalContainer).start();
---
+++ terminal.ts
@@ -0,0 +1,153 @@
+import "@xterm/xterm/css/xterm.css";
+import "./terminal.css";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
+import LocalEchoController from "./local-echo";
+
+export function createTerminal(domElement: HTMLElement) {
+    const terminal = createXtermTerminal(domElement);
+
+    setupKeyboardExtension(terminal, (button) => {
+        terminal.textarea.dispatchEvent(new KeyboardEvent("keydown", button));
+        terminal.focus();
+    });
+
+    return setupLocalEcho(terminal, console.log);
+}
+
+function createXtermTerminal(domElement: HTMLElement) {
+    const terminal = new Terminal();
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+    terminal.open(domElement);
+
+    window.addEventListener("resize", () => fitAddon.fit());
+    fitAddon.fit();
+
+    return terminal;
+}
+
+function setupLocalEcho(
+    terminal: Terminal,
+    onCommand: (cmd: string) => void | Promise<void>,
+) {
+    const localEcho = new LocalEchoController(terminal);
+
+    const loop = async () => {
+        try {
+            const cmd = await localEcho.read("~$ ");
+            const maybePromise = onCommand(cmd);
+            if (maybePromise instanceof Promise) await maybePromise;
+        } catch (e) {
+            console.log(e);
+        }
+
+        loop();
+    };
+
+    return {
+        start: loop,
+    };
+}
+
+type ButtonClickParam = {
+    key: string;
+    keyCode: number;
+    code: string;
+    which: number;
+};
+
+function setupKeyboardExtension(
+    terminal: Terminal,
+    onButtonClick: (p: ButtonClickParam) => void,
+) {
+    const keyboardExtension = document.createElement("div");
+    keyboardExtension.classList.add("keyboard-extension");
+
+    keyboardExtensionOverlayHeightListener(keyboardExtension);
+    keyboardExtensionShowHideOnTerminalFocus(terminal, keyboardExtension);
+
+    const inner = document.createElement("div");
+
+    const tabButton = createButton("tab", "Tab", 9, onButtonClick);
+    const leftButton = createButton("<", "ArrowLeft", 37, onButtonClick);
+    const rightButton = createButton(">", "ArrowRight", 39, onButtonClick);
+
+    inner.append(tabButton, leftButton, rightButton);
+    keyboardExtension.append(inner);
+
+    document.body.append(keyboardExtension);
+}
+
+function createButton(
+    text: string,
+    key: ButtonClickParam["key"],
+    keyCode: ButtonClickParam["keyCode"],
+    onClick: (p: ButtonClickParam) => void,
+) {
+    const button = document.createElement("button");
+    button.innerText = text;
+    button.onclick = () =>
+        onClick({
+            key,
+            keyCode,
+            code: key,
+            which: keyCode,
+        });
+    return button;
+}
+
+function keyboardExtensionOverlayHeightListener(
+    keyboardExtElement: HTMLElement,
+) {
+    let lastHeight = 0;
+    const checkHeight = () => {
+        const currentHeight = window.visualViewport.height;
+
+        if (currentHeight !== lastHeight) {
+            keyboardExtElement.style.height = currentHeight + "px";
+        }
+
+        lastHeight = currentHeight;
+
+        window.requestAnimationFrame(checkHeight);
+    };
+    checkHeight();
+}
+
+function keyboardExtensionShowHideOnTerminalFocus(
+    terminal: Terminal,
+    keyboardExtElement: HTMLElement,
+) {
+    let hideThrottler: ReturnType<typeof setTimeout>;
+    const hideKeyboardExt = () => {
+        if (hideThrottler) {
+            clearTimeout(hideThrottler);
+        }
+        hideThrottler = setTimeout(() => {
+            keyboardExtElement.classList.remove("show");
+            hideThrottler = null;
+        }, 100);
+    };
+
+    let lastFocusedElement = null;
+    const checkFocus = () => {
+        const currentFocusedElement = document.activeElement;
+
+        if (currentFocusedElement !== lastFocusedElement) {
+            if (currentFocusedElement === terminal.textarea) {
+                if (hideThrottler) {
+                    clearTimeout(hideThrottler);
+                }
+                keyboardExtElement.classList.add("show");
+            } else {
+                hideKeyboardExt();
+            }
+        }
+
+        lastFocusedElement = currentFocusedElement;
+
+        window.requestAnimationFrame(checkFocus);
+    };
+    checkFocus();
+}
